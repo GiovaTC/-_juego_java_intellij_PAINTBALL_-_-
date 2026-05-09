9e8f6dc761cbdaf1a54a203a09e4189dd1ce2055
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-09 14:47:21 -0500

    commit - Bullet / .

--- src/Bullet.java
+++ src/Bullet.java
@@ -1,2 +1,30 @@
+import java.awt.*;
+
 public class Bullet {
-}
+
+    int x;
+    int y;
+
+    int width = 10;
+    int height = 5;
+    int speed = 10;
+
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void move() {
+        x += speed;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillOval(x, y, width, height);
+    }
+
+    public Rectangle getBounds() {
+
+        return new Rectangle(x, y, width, height);
+    }
+}   
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,22 @@
+import java.awt.*;
+
 public class Player {
+
+    int x;
+    int y;
+
+    int width = 40;
+    int height = 40;
+    int speed = 5;
+
+    public Player(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, width, height);
+
+    }
 }
