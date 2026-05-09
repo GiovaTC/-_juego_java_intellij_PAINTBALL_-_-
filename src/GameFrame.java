5a39d37087ca738f201f5fe2d89e215d35751792
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-09 14:40:36 -0500

    commit - GameFrame / .

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,2 +1,21 @@
-public class GameFrame {
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GamePanel panel;
+
+    GameFrame() {
+
+        panel = new GamePanel();
+
+     //   this.add(panel);
+
+        this.setTitle("PAINTBALL GAME");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
 }
