916e79206b555d85ff1a95396f515e49fa7fb677
gtapiero <giovanny.tapiero@pi.edu.co>
2026-05-09 15:15:17 -0500

    commit - Enemy .

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,16 +1,42 @@
 import java.awt.*;
+import java.util.Random;
 
 public class Enemy {
-    public Enemy(int i, int y) {
+
+    int x;
+    int y;
+
+    int width = 40;
+    int height = 40;
+
+    int speed = 3;
+
+    Random random = new Random();
+
+    public Enemy(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
     }
 
     public void move() {
+
+        x -= speed;
+
     }
 
     public void draw(Graphics g) {
+
+        g.setColor(Color.RED);
+        g.fillRect(x, y, width, height);
+
     }
 
     public Rectangle getBounds() {
-        return null;
+
+        return new Rectangle(x, y, width, height);
+
     }
+
 }
